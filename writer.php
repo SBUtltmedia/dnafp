d1132734ba2508cc45f9206c1158b56c4e32238d
APeitzsch <agpeitzsch@gmail.com>
2019-01-07 16:39:31 -0500

    Changes to the players database

--- writer.php
+++ writer.php
@@ -3,11 +3,8 @@ $a = new stdClass();
 $a -> studentData = $_REQUEST['studentData'];
 $a -> stats = json_decode($_REQUEST['stats']);
 $a -> netid =$_SERVER['cn'];
-if ($a -> studentData >0)
-{
 $info= "${_SERVER['cn']},${_REQUEST['studentData']}\n";   
 file_put_contents("data/winners.csv",$info, FILE_APPEND | LOCK_EX); 
-}
 
 $a = json_encode($a);
 file_put_contents("data/".$_SERVER['cn'],$a);
